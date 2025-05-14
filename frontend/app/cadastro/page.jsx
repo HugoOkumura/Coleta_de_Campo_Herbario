b987344233ad7c8002292861db4034355667f65b
Guilherme Nishida <gnishida81@gmail.com>
2025-05-14 15:05:18 -0300

    fazendo a tela de cadastro de planta

---
+++ frontend/app/cadastro/page.jsx
@@ -0,0 +1,10 @@
+// /app/cadastro/page.jsx
+import FormularioExpedicao from '../componentes/FormularioExpedicao';
+
+export default function CadastroPage() {
+  return (
+    <div style={{ backgroundColor: '#f1f7f6', minHeight: '100vh', paddingTop: '40px' }}>
+      <FormularioExpedicao />
+    </div>
+  );
+}
---
+++ frontend/app/planta/page.jsx
@@ -0,0 +1,10 @@
+// /app/plantas/page.jsx
+import FormularioPlanta from '../componentes/FormularioPlanta';
+
+export default function PlantaPage() {
+  return (
+    <div style={{ backgroundColor: '#f1f7f6', minHeight: '100vh', paddingTop: '40px' }}>
+      <FormularioPlanta />
+    </div>
+  );
+}
